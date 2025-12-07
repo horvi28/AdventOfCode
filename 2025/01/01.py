5abbf6c7faa5f17c7c650cb502ee2557ae67b874
Horvath Zsolt <horvi28@gmail.com>
2025-12-07 18:25:54 +0100

    Second part of the 1st puzzle
    I know it is an ugly solution, I will make it cleaner later

--- 2025/01/01.py
+++ 2025/01/01.py
@@ -1,8 +1,10 @@
 # https://adventofcode.com/2025/day/1
 
-with open('./2025/01/testinput.txt', 'r') as f:
+with open('./2025/01/input.txt', 'r') as f:
     dial = 50 # The dial starts at 50
-    dial_is_zero_counter = 0
+    dial_exactly_at_zero_counter = 0 # Count if the dial stopping exactly at zero
+    dial_underflow = 0 # Count if the dial passing zero while rotating to the left
+    dial_overflow = 0 # Count if the dial passing zero while rotating to the right
     for line in f:
         line = line.rstrip()
         direction = line[0]
@@ -10,11 +12,15 @@ with open('./2025/01/testinput.txt', 'r') as f:
         
         # Rotate to the dial to the left (lower numbers)
         if direction == 'L':
+            # If the dial is exactly at zero, that should not count as underflow, so compensated here
+            if dial == 0:
+                dial_underflow -= 1
             dial -= rotation
             
             # Handle underflow
             while dial < 0:
                 dial += 100
+                dial_underflow += 1
         # Rotate the dial to the right (higher numbers)
         else:
             dial += rotation
@@ -22,9 +28,17 @@ with open('./2025/01/testinput.txt', 'r') as f:
             # Handle overflow
             while dial > 99:
                 dial -= 100
-        
+                # 100 is equal with zero, do not count when it finishes at zero
+                if dial != 0:
+                    dial_overflow += 1
+
         if dial == 0:
-            dial_is_zero_counter += 1
+            dial_exactly_at_zero_counter += 1
 
-print(f"The dial has hit zero {dial_is_zero_counter} times")
+dial_total_zero_hit = dial_exactly_at_zero_counter + dial_underflow + dial_overflow
 
+print("The dial hit:")
+print(f"Exactly zero: {dial_exactly_at_zero_counter}")
+print(f"Underflow: {dial_underflow}")
+print(f"Overflow: {dial_overflow}")
+print(f"Total: {dial_total_zero_hit}")
