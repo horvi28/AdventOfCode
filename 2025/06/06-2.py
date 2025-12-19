c5c9f4200f5ab568a7d88f74631ea8e2022f819e
Horvath Zsolt <horvi28@gmail.com>
2025-12-19 15:52:37 +0100

    Refactor a bit the second part of the 6th puzzle to make it more clear

--- 2025/06/06-2.py
+++ 2025/06/06-2.py
@@ -1,5 +1,6 @@
 import math
 
+
 def get_next_operator(line: str, offset: int) -> tuple[str, int]:
     operators_look_for = ['+', '*']
     for index, character in enumerate(line[offset:], offset):
@@ -7,45 +8,60 @@ def get_next_operator(line: str, offset: int) -> tuple[str, int]:
             return character, index
     return '', -1
 
+
+def get_vertical_numbers_for_sub_result(lines: list[str], offset: int, number_size) -> list[int]:
+    numbers = []
+    for digit_index in range(offset, offset + number_size):
+        number = ''
+        for line in lines:
+            number += line[digit_index]
+        numbers.append(int(number))
+    return numbers
+
+
+def calculate_sub_result(numbers: list[int], operator: str) -> int:
+        if operator == '+':
+            sub_result = sum(numbers)
+        else:
+            sub_result = math.prod(numbers)
+        
+        print(f'Sub result is {sub_result}')
+        return sub_result
+
+
 with open('./2025/06/input.txt', 'r') as f:
     lines = f.readlines()
     
     # Process the input based on the last line
     # Every column starts with the operator
+    operator_row = lines[-1]
+    
     offset = 0
-    operator = lines[-1][0]
-
+    operator = operator_row[0]
     total = 0
     there_are_still_operator = True
     while there_are_still_operator:
-        next_operator, next_offset = get_next_operator(lines[-1], offset + 1)
+        # Find the next operator and offset value
+        next_operator, next_offset = get_next_operator(operator_row, offset + 1)
         
-        # No more operator, so we are at the end
-        if next_operator == '' and next_offset == -1:
-            there_are_still_operator = False
-            numbers_size = len(lines[-1]) - offset
-        else:
-            # Get how long numbers we have in the current column
+        if next_operator != '' and next_offset != -1:
+            # Get how long numbers we have in the current sub problem
             # This can be calculated based on the distance between the operators
-            # minus one as there is always spaces between the columns
-            numbers_size = next_offset - offset - 1
+            # minus one as there is always spaces between the problems
+            number_size = next_offset - offset - 1
+        else:
+            # No more operator, so we are at the end
+            # And we also have to calculate the number size differently here
+            there_are_still_operator = False
+            number_size = len(operator_row) - offset
 
         # Get the vetical numbers into a list
-        numbers = []
-        for digit_index in range(offset, offset + numbers_size):
-            number = ''
-            for line in lines[:-1]:
-                number += line[digit_index]
-            numbers.append(int(number))
+        numbers = get_vertical_numbers_for_sub_result(lines[:-1], offset, number_size)
 
-        # Calculate the sub result
-        if operator == '+':
-            sub_result = sum(numbers)
-        else:
-            sub_result = math.prod(numbers)
-        total += sub_result
-        print(f'Sub result is {sub_result}')
+        # Calculate the sub result and it to the total
+        total += calculate_sub_result(numbers, operator)
 
+        # Prepare for the next iteration
         operator = next_operator
         offset = next_offset
 print(f"Total sum of sub results: {total}")
