3dd8a42e25ddf5bd058b5b6d44af5a4abc9561a3
Horvath Zsolt <horvi28@gmail.com>
2025-12-21 11:35:43 +0100

    Add 7th puzzle part 2

--- 2025/07/07.py
+++ 2025/07/07.py
@@ -1,8 +1,9 @@
 # Plan:
-# Find the S where the tachyon enters, mark it's position (row,column)
-# Store the column in a tachyons set (this will make sure that no duplicates)
+# Find the S where the tachyon enters, mark it's position (row, column)
+# Store the column and timeline count in a dictionary, 
+#   where the key is the column index, and the value is the timelines for that column
 # Jump down 2, see if there is any splitter
-# Split the beams
+# Split the beams (each timeline at a splitter becomes 2 timelines)
 # Repeat
 
 from grid import Grid
@@ -11,24 +12,33 @@ with open('./2025/07/input.txt', 'r') as f:
     lines = f.readlines()
     manifold = Grid.from_lines(lines)
 
-    # Find S and put into the tachyons set
+    # Find S and put into the tachyons dict with 1 timeline
     row = 0
     index = lines[row].find('S')
-    tachyons = set()
-    tachyons.add(index)
+    tachyons = {}  # key: column index of the beam -> value: number of timelines for that column
+    tachyons[index] = 1
 
     split_counter = 0
     total_rows, toltal_columns = manifold.get_dimensions()
     while row <= total_rows - 2:
         row += 2
 
-        # Go through the set, check if there is any splitter
+        # Go through the dict, check if there is any splitter
         old_tachyons = tachyons.copy()
-        for column in old_tachyons:
+        for column, count in old_tachyons.items():
             # If there is a splitter, then split the beam
             if manifold.get(row, column) == '^':
-                tachyons.add(column - 1)
-                tachyons.add(column + 1)
-                tachyons.remove(column)
                 split_counter += 1
-print(f'The tachyon beams are splitted {split_counter} times')
+
+                # The new column timeline will be the sum of the previous + the new possibilities to end up here
+                tachyon_left = column - 1
+                tachyons[tachyon_left] = tachyons.get(tachyon_left, 0) + count
+
+                tachyon_right = column + 1
+                tachyons[tachyon_right] = tachyons.get(tachyon_right, 0) + count
+
+                # Remove the original position
+                del tachyons[column]
+
+    timeline_counter = sum(tachyons.values())
+print(f'The tachyon beams are splitted {split_counter} times and there are {timeline_counter} timelines')
